517b864d412235938bee6568c60854f5c18f905d
Vishalcj17 <vishalcj17@gmail.com>
2021-08-18 23:32:11 +0530

    elytra: product: Include Snapdragon Clang makefile
    Change-Id: Ibb54342994005cdd43fc0f185e505573b6e3b6a3

---
+++ target/product/elytra-target.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 elytraOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Include definitions for Snapdragon Clang
+$(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
