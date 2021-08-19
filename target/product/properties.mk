9b83a010a023553ffa470e78db131e1abd144e16
Vishalcj17 <vishalcj17@gmail.com>
2021-08-19 11:19:38 +0530

    elytra: properties: Enforce priv-app permissions
    Change-Id: I131655cc0e56569036126c4a7e9695606c21b37b

---
+++ target/product/properties.mk
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2022 elytraOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enforce privapp-permissions whitelist.
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.control_privapp_permissions=enforce
