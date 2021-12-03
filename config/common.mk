VoltageOS/vendor_voltage

vendor: build fonts from our vendor
Signed-off-by: Dmitrii <bankersenator@gmail.com>

config/common.mk
@@ -119,5 +119,8 @@ include vendor/voltage/config/version.mk
 # BootAnimation
 -include vendor/voltage/config/bootanimation.mk
 
+# Fonts
+$(call inherit-product, vendor/voltage/fonts/fonts.mk)
+
 # Include extra packages
 include vendor/voltage/config/packages.mk

fonts/font_files.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/proton/fonts
+LOCAL_PATH := vendor/voltage/fonts
 
 # We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
 # PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
@@ -41,4 +41,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
 
 # Include extra addon fonts
--include vendor/proton/fonts/extra/extra.mk
+-include vendor/voltage/fonts/extra/extra.mk

fonts/fonts.mk
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/proton/fonts
+LOCAL_PATH := vendor/voltage/fonts
 
 # Copy font files
-include vendor/proton/fonts/font_files.mk
--include vendor/proton/fonts/extra/extra.mk
+include vendor/voltage/fonts/font_files.mk
+-include vendor/voltage/fonts/extra/extra.mk
 
 # Register custom fonts
 PRODUCT_COPY_FILES += \

fonts/generate_build.sh
@@ -21,7 +21,7 @@ echo '# Copyright (C) 2021 The Proton AOSP Project
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/proton/fonts
+LOCAL_PATH := vendor/voltage/fonts
 
 # We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
 # PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
@@ -89,4 +89,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
 
 # Include extra addon fonts
--include vendor/proton/fonts/extra/extra.mk' >> font_files.mk
+-include vendor/voltage/fonts/extra/extra.mk' >> font_files.mk

fonts/generate_overlays.py
@@ -58,7 +58,7 @@ ANDROID_MANIFEST_TEMPLATE = """<!--
     limitations under the License.
 -->
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="org.protonaosp.theme.font.{pkg_name}">
+    package="org.voltage.theme.font.{pkg_name}">
 
     <overlay android:targetPackage="android"
         android:category="android.theme.customization.font"

elytraOS/vendor_elytra

config/common.mk
@@ -128,5 +128,8 @@ include vendor/elytra/config/version.mk
 # BootAnimation
 include vendor/elytra/config/bootanimation.mk
 
+# Fonts
+$(call inherit-product, vendor/elytra/fonts/fonts.mk)
+
 # Include extra packages
 include vendor/elytra/config/packages.mk

fonts/font_files.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/proton/fonts
+LOCAL_PATH := vendor/elytra/fonts
 
 # We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
 # PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
@@ -41,4 +41,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
 
 # Include extra addon fonts
--include vendor/proton/fonts/extra/extra.mk
+-include vendor/elytra/fonts/extra/extra.mk

fonts/fonts.mk
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/proton/fonts
+LOCAL_PATH := vendor/elytra/fonts
 
 # Copy font files
-include vendor/proton/fonts/font_files.mk
--include vendor/proton/fonts/extra/extra.mk
+include vendor/elytra/fonts/font_files.mk
+-include vendor/elytra/fonts/extra/extra.mk
 
 # Register custom fonts
 PRODUCT_COPY_FILES += \

fonts/generate_build.sh
@@ -21,7 +21,7 @@ echo '# Copyright (C) 2021 The Proton AOSP Project
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/proton/fonts
+LOCAL_PATH := vendor/elytra/fonts
 
 # We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
 # PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
@@ -89,4 +89,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
 
 # Include extra addon fonts
--include vendor/proton/fonts/extra/extra.mk' >> font_files.mk
+-include vendor/elytra/fonts/extra/extra.mk' >> font_files.mk

fonts/generate_overlays.py
@@ -58,7 +58,7 @@ ANDROID_MANIFEST_TEMPLATE = """<!--
     limitations under the License.
 -->
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
-    package="org.protonaosp.theme.font.{pkg_name}">
+    package="org.elytra.theme.font.{pkg_name}">
 
     <overlay android:targetPackage="android"
         android:category="android.theme.customization.font"