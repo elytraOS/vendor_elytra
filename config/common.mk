64a23ce4f2c74e101f1953fc3161e70a2fc58ef9
PainKiller3 <ninadpatil100@gmail.com>
2021-12-22 13:17:07 +0100

    Revert "vendor: Add SettingsIntelligenceGooglePrebuilt"
    This reverts commit 368f05242157d29dee2a3bcd230087a51bc7d453.
    
    Change-Id: I6faa0b5a358c4c2216c0eb0711321f9b11dbffdc

--- config/common.mk
+++ config/common.mk
@@ -129,8 +129,7 @@ endif
 
 # Config
 PRODUCT_PACKAGES += \
-    SimpleDeviceConfig \
-    SettingsIntelligenceGooglePrebuilt
+    SimpleDeviceConfig
 
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -139,10 +138,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # These packages are excluded from user builds
 PRODUCT_PACKAGES_DEBUG += \
     procmem
-
-# Permissions
-PRODUCT_COPY_FILES += \
-    vendor/elytra/config/permissions/privapp-permissions-elytra-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-elytra.xml
 
 # Extra tools
 PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
