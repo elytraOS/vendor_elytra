24bf27cb1dd1047727db634413374e9a3b3b0af8
kondors1995 <normandija1945@gmail.com>
2022-02-18 11:39:15 +0000

    build: Enable GlobalThinLTO & ThinLTO cache
    Change-Id: Id296419122d7ebba5212904d36780e40ce41a7a0

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -948,6 +948,11 @@ function fixup_common_out_dir() {
     fi
 }
 
+# Enable ThinLTO Source wide.
+echo "Building with ThinLTO."
+export GLOBAL_THINLTO=true
+export USE_THINLTO_CACHE=true
+
 # For now, just skip the ABI checks to fix build errors.
 export SKIP_ABI_CHECKS=true
 
