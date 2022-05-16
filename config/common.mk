Project-Awaken/android_vendor_awaken

vendor: Move pixel sounds to GMS
* We're now using them for gapped builds only
* Also guard AOSP sounds while we're at it

This reverts commit af1be283d3fcd2191dda98a296288885a23100b2.

config/common.mk
@@ -125,8 +125,11 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_land_l=64 \
     ro.com.google.ime.kb_pad_land_r=64
 
-# Include Pixel audio files
-include vendor/awaken/config/audio.mk
+# Include AOSP audio files
+include vendor/awaken/config/aosp_audio.mk
+
+# Include Awaken audio files
+include vendor/awaken/config/awaken_audio.mk
 
 # Include extra packages
 include vendor/awaken/config/packages.mk

elytraOS/vendor_elytra

config/common.mk
@@ -112,6 +112,12 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Optimize everything for preopt
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
+# Include AOSP audio files
+include vendor/elytra/config/aosp_audio.mk
+
+# Include elytra audio files
+include vendor/elytra/config/elytra_audio.mk
+
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
@@ -177,9 +183,6 @@ include vendor/elytra/config/bootanimation.mk
 
 # Fonts
 $(call inherit-product, vendor/elytra/fonts/fonts.mk)
-
-# Audio
-$(call inherit-product, vendor/elytra/audio/audio.mk)
 
 # Include extra packages
 include vendor/elytra/config/packages.mk