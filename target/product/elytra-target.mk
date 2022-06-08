5531b4b197ebfe7d2451df795e99602b03bcf7bc
Chris Crump <chriscrawford893@gmail.com>
2022-06-08 01:12:15 -0400

    elytra: Move APN/SPN configurations to vendor/elytra
    These are needed for all devices and should not be Qualcomm specific
    
    Change-Id: I6fea2c2dce95a8fd9f260e5187d8a91f46e0d4fe

--- target/product/elytra-target.mk
+++ target/product/elytra-target.mk
@@ -57,6 +57,11 @@ PRODUCT_COPY_FILES += \
     vendor/elytra/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/elytra/target/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Sensitive phone numbers and APN configurations
+PRODUCT_COPY_FILES += \
+    vendor/elytra/target/config/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
+    vendor/elytra/target/config/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
+
 # Skip boot JAR checks
 SKIP_BOOT_JARS_CHECK := true
 
