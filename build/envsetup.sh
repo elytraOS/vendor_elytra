e9b8f74d2f57702b602936e7acbeebe54805a06a
ReallySnow <reallysnow233@gmail.com>
2022-10-20 23:22:45 +0800

    elytra: tools: Initial repopick from LineageOS
    * Sync https://github.com/LineageOS/android_vendor_lineage/commit/39881f863a5644c0227d8935d64edd97af6b4644
    * Rebrand to elytra
    
    Change-Id: I676716fc7a3ca769df90474c61c677995468d2ef

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -8,9 +8,16 @@ function __print_elytra_functions_help() {
 cat <<EOF
 Additional elytra functions:
 - mka:             Builds using SCHED_BATCH on all processors.
+- repopick:        Utility to fetch changes from Gerrit.
 EOF
 }
 
 function mka() {
     m -j "$@"
+}
+
+function repopick()
+{
+    T=$(gettop)
+    $T/vendor/elytra/build/tools/repopick.py $@
 }
