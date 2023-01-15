f919f45193ea544551b7ea6ab00293dca929d7d4
cjh1249131356 <cjh1249131356@gmail.com>
2023-01-15 17:52:32 +0800

    soong: Add TARGET_USES_NOTHING_CAMERA
    Change-Id: I7bc263a270d24217416b064d6ce4e9e950568988
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -22,7 +22,8 @@ SOONG_CONFIG_elytraGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
-    uses_oplus_camera
+    uses_oplus_camera \
+    uses_nothing_camera
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
@@ -35,6 +36,7 @@ SOONG_CONFIG_elytraGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET
 SOONG_CONFIG_elytraGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_elytraGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
 SOONG_CONFIG_elytraGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_elytraGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 
 # Gestures
 define add-gesturevar-if-exist
