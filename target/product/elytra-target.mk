ab92e0147d82d23a7c651a39f5f7d0b9a17c3acd
Jake Weinstein <jake@aospa.co>
2023-02-14 10:15:53 +0900

    elytra: Reorganize target makefiles
    There's really no point in separating packages
    and properties.
    
    Change-Id: Iece79a3b2e744a110f73e99200f60716ef369eb2

--- target/product/elytra-target.mk
+++ target/product/elytra-target.mk
@@ -4,31 +4,67 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# elytra Versioning
-$(call inherit-product, vendor/elytra/target/product/version.mk)
+# APNs
+PRODUCT_COPY_FILES += \
+    vendor/elytra/target/config/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
+
+# Audio
+# Increase volume level steps
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.config.media_vol_steps=30
+
+# Blurs
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
 
 # Bootanimation
 $(call inherit-product, vendor/elytra/bootanimation/bootanimation.mk)
 
+# Camera
+PRODUCT_PACKAGES += \
+    GoogleCameraGo
+
+# Curl
+PRODUCT_PACKAGES += \
+    curl
+
+# Dex2oat
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true
+
+# Dexpreopt
 # Don't dexpreopt prebuilts. (For GMS).
 DONT_DEXPREOPT_PREBUILTS := true
+
+PRODUCT_DEXPREOPT_SPEED_APPS += \
+    ElytraSystemUI
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.systemuicompilerfilter=speed
+
+# Display
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=0
+
+# elytra Versioning
+$(call inherit-product, vendor/elytra/target/product/version.mk)
+
+# Exfat FS
+PRODUCT_PACKAGES += \
+    fsck.exfat \
+    mkfs.exfat
 
 # Fonts
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/elytra/prebuilts/fonts/,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
     vendor/elytra/target/config/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
-# HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-     vendor/elytra/target/config/elytra_vendor_framework_compatibility_matrix.xml
+# Gestures
+PRODUCT_PACKAGES += \
+    vendor.elytra.power-service
 
-# Include Common Qualcomm Device Tree
-$(call inherit-product, device/qcom/common/common.mk)
-
-# Include definitions for Snapdragon Clang
-$(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
-
-# Include GMS, Modules and Pixel features
+# Google - GMS, Pixel, and Mainline Modules
 $(call inherit-product, vendor/google/gms/config.mk)
 $(call inherit-product, vendor/google/pixel/config.mk)
 
@@ -38,20 +74,45 @@ else
 $(call inherit-product-if-exists, vendor/google/modules/build/mainline_modules_flatten_apex.mk)
 endif
 
-# Include Overlay makefile
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+     vendor/elytra/target/config/elytra_vendor_framework_compatibility_matrix.xml
+
+# HIDL
+PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.manager@1.0 \
+    android.hidl.base@1.0.vendor \
+    android.hidl.manager@1.0.vendor
+
+# Java Optimizations
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+SYSTEM_OPTIMIZE_JAVA ?= true
+SYSTEMUI_OPTIMIZE_JAVA ?= true
+
+# MTE
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    arm64.memtag.process.system_server=off
+
+# Navigation
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural
+
+# Neural Network
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-rtti
+
+# One Handed Mode
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.support_one_handed_mode=true
+
+# Overlays
 $(call inherit-product, vendor/elytra/overlay/overlays.mk)
 
-# Include Packages makefile
-$(call inherit-product, vendor/elytra/target/product/packages.mk)
-
-# Include Properties makefile
-$(call inherit-product, vendor/elytra/target/product/properties.mk)
-
-# Include SEPolicy makefile.
-$(call inherit-product, vendor/elytra/sepolicy/sepolicy.mk)
-
-# Move Wi-Fi modules to vendor
-PRODUCT_VENDOR_MOVE_ENABLED := true
+# Elytra Packages
+PRODUCT_PACKAGES += \
+    ElytraThemePicker \
+    ElytraSystemUI
 
 # Permissions
 PRODUCT_COPY_FILES += \
@@ -59,25 +120,72 @@ PRODUCT_COPY_FILES += \
     vendor/elytra/target/config/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
     vendor/elytra/target/config/permissions/lily_experience.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/lily_experience.xml
 
-# Pre-optimization
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-    ElytraSystemUI
+# Privapp-permissions
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.control_privapp_permissions=enforce
 
-# Compile SystemUI on device with `speed`.
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.systemuicompilerfilter=speed
+# QTI VNDK Framework Detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti \
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect_system \
+    libqti_vndfwk_detect_vendor \
+    libvndfwk_detect_jni.qti_system \
+    libvndfwk_detect_jni.qti_vendor \
+    libvndfwk_detect_jni.qti.vendor \
+    libqti_vndfwk_detect.vendor
 
-# Sensitive phone numbers and APN configurations
+# Qualcomm Common
+$(call inherit-product, device/qcom/common/common.mk)
+
+# Rescue Party
+# Disable RescueParty due to high risk of data loss
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.disable_rescue=true
+
+# Sensitive Phone Numbers
 PRODUCT_COPY_FILES += \
-    vendor/elytra/target/config/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
     vendor/elytra/target/config/sensitive_pn.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sensitive_pn.xml
 
-# Strip the local variable table and the local variable type table to reduce
-# the size of the system image. This has no bearing on stack traces, but will
-# leave less information available via JDWP.
-PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+# SEPolicy
+$(call inherit-product, vendor/elytra/sepolicy/sepolicy.mk)
 
-# Enable whole-program R8 Java optimizations for SystemUI and system_server,
-# but also allow explicit overriding for testing and development.
-SYSTEM_OPTIMIZE_JAVA ?= true
-SYSTEMUI_OPTIMIZE_JAVA ?= true
+# Snapdragon Clang
+$(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
+
+# Telephony - AOSP
+PRODUCT_PACKAGES += \
+    Stk
+
+# Telephony - CLO
+PRODUCT_PACKAGES += \
+    extphonelib \
+    extphonelib-product \
+    extphonelib.xml \
+    extphonelib_product.xml \
+    ims-ext-common \
+    ims_ext_common.xml \
+    tcmiface \
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti-telephony-hidl-wrapper-prd \
+    qti_telephony_hidl_wrapper.xml \
+    qti_telephony_hidl_wrapper_prd.xml \
+    qti-telephony-utils \
+    qti-telephony-utils-prd \
+    qti_telephony_utils.xml \
+    qti_telephony_utils_prd.xml
+
+PRODUCT_BOOT_JARS += \
+    tcmiface \
+    telephony-ext
+
+# Wallpapers
+PRODUCT_PACKAGES += \
+    Covers
+
+# WiFi
+PRODUCT_PACKAGES += \
+    libwpa_client
+
+PRODUCT_VENDOR_MOVE_ENABLED := true
