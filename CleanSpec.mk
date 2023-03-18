5edc5889f270a6ac45eec7509249e8cc276bccd0
BladeRunner-A2C <prisoner56898@protonmail.com>
2023-03-18 13:58:08 +0600

    elytra: Remove clean spec makefile
     * not needed after https://github.com/AOSPA/android_build_soong/commit/afcbc334f278b1b503c23392c0b1edd4756e127c
    
    Change-Id: Ie345fe439291f846bbf5b4f55150d0da9df566d5
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- CleanSpec.mk
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2022 elytraOS
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/kernel)
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/KERNEL_OBJ)
