PixelOS-AOSP/vendor_aosp

vendor: Adapt clock font to qpr1
Change-Id: Ie369cdc9e1f491e2326036e90270c625b04452b6
Reviewed-on: https://review.statixos.com/c/android_vendor_statix/+/9118
Reviewed-by: Anay Wadhera <anay1018@gmail.com>
Tested-by: Anay Wadhera <anay1018@gmail.com>
Reviewed-by: Sourajit Karmakar <sourajitk@statixos.com>

config/fonts.mk
@@ -2,4 +2,5 @@
 PRODUCT_COPY_FILES += \
     vendor/aosp/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/aosp/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/aosp/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
     vendor/aosp/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml

elytraOS/vendor_elytra

config/fonts.mk
@@ -2,4 +2,5 @@
 PRODUCT_COPY_FILES += \
     vendor/elytra/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
     vendor/elytra/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
+    vendor/elytra/fonts/GoogleSansClock-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSansClock-Regular.ttf \
     vendor/elytra/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml